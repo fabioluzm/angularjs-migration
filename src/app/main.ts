8109427a1532fc441918298254f5fe8122166dcb
fabioluzm <fabio.luzm@gmail.com>
2023-06-06 18:57:36 +0100

    add dual booting with Angular JS and Angular

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,16 +1,42 @@
-import 'angular';
-import 'angular-resource';
-import 'angular-animate';
-import 'ng-infinite-scroll';
-import 'angular-spinner';
-import 'angular-auto-validate/dist/jcs-auto-validate';
-import 'angular-ladda';
-import 'angular-strap';
-import 'angularjs-toaster';
-import 'angular-ui-router';
+import "angular";
+import "angular-resource";
+import "angular-animate";
+import "ng-infinite-scroll";
+import "angular-spinner";
+import "angular-auto-validate/dist/jcs-auto-validate";
+import "angular-ladda";
+import "angular-strap";
+import "angularjs-toaster";
+import "angular-ui-router";
 
-import './app.main';
-import './services';
-import './filters';
-import './components';
-import './app.routes';
+import "./app.main";
+import "./services";
+import "./filters";
+import "./components";
+import "./app.routes";
+
+import "./polyfills.ts";
+
+// Import Angular modules
+
+import {NgModule} from "@angular/core";
+import {BrowserModule} from "@angular/platform-browser";
+import {UpgradeModule} from "@angular/upgrade/static";
+import {platformBrowserDynamic} from "@angular/platform-browser-dynamic";
+
+@NgModule({
+  imports: [BrowserModule, UpgradeModule],
+})
+export class AppModule {
+  // Override Angular bootstrap so it doesn't do anything
+  ngDoBootstrap() {}
+}
+
+// Bootstrap using the UpgradeModule
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .then(platformRef => {
+    console.log("Bootstrapping in Hybrid mode with Angular & AngularJS");
+    const upgrade = platformRef.injector.get(UpgradeModule) as UpgradeModule;
+    upgrade.bootstrap(document.body, ["codecraft"]);
+  });
